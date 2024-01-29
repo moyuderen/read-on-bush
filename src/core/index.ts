a88b9f7c3c1a6958d1cc3541a3e75311622d4bd6
moyuderen <moyuderen>
2024-01-29 17:41:41 +0800

    feat: import按钮调整位置

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,12 +2,14 @@ import { type ExtensionContext } from 'vscode';
 import { ReadBook } from "./ReadBook";
 import { setupStorage } from '../utils/storage';
 import { setupBars } from './barItems';
+import { setupViewTitleImport } from './views';
 
 export let app: ReadBook;
 
 export function setup(context: ExtensionContext) {
   setupStorage(context);
   setupBars(context);
+  setupViewTitleImport(context);
 
   app = new ReadBook(context);
   return app;
