73997ee77d9e58fe9af434d4c0f94071ec0a5be9
moyuderen <moyuderen>
2024-01-31 15:14:47 +0800

    feat: tooltip支持复制

--- src/core/barItems/content.ts
+++ src/core/barItems/content.ts
@@ -1,12 +1,28 @@
 
-import { window } from 'vscode';
+import { window, MarkdownString } from 'vscode';
 import type { StatusBarItem, ExtensionContext } from 'vscode';
+import { app } from '../index';
 
 export let contentBarItem: StatusBarItem;
 
 export function updateContent(content: string) {
   contentBarItem.text = content;
-  contentBarItem.tooltip = content;
+  // contentBarItem.tooltip = content;
+  // 需要 [链接](http://) 来块化tooltip, 起到hover不隐藏的目的
+  const mk = ` 
+  |      |
+  | :--- |
+  | []() ${content} |
+  `;
+  const tooltip = new MarkdownString(mk, true);
+  tooltip.supportHtml = true;
+  tooltip.isTrusted = true;
+
+  if(app && app.readingBook) {
+    tooltip.appendMarkdown(`\n\n---\n\n《${app.readingBook.book.name}》`);
+  }
+
+  contentBarItem.tooltip = tooltip;
 }
 
 export function setupContentBarItem(context: ExtensionContext) {
